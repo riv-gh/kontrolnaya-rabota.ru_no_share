2f1664481fb3e6cafc156007f498bdfc2340698c
daenur <daenur@outlook.com>
2019-12-02 12:27:48 +0200

    Create console.js

---
+++ console.js
@@ -0,0 +1,4 @@
+//copy past this on console when bloor
+var style_noBloor = document.createElement('style');
+style_noBloor.innerText = "#steps_0>span{color:black!important;text-shadow:none!important}";
+document.getElementsByTagName('head')[0].appendChild(style_noBloor);
