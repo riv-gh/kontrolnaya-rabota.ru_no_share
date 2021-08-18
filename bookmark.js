91701a329bc45543f73584ce1d63ace566c5b16d
daenur <daenur@outlook.com>
2021-08-18 16:59:23 +0300

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,1 @@
+javascript:(function(){var style_noBloor=document.createElement('style');style_noBloor.innerText="#steps_0>span{color:black!important;text-shadow:none!important} .krapi-blurred-title{display:none!important}";document.getElementsByTagName('head')[0].appendChild(style_noBloor);})()
